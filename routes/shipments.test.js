0a21be791ff6329186b4ed0896064f97dcbd0314
lancelee2885 <lancelee2885@gmail.com>
2021-08-03 17:20:36 -0700

    shipItApi test completed

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,6 +3,11 @@
 const request = require("supertest");
 const app = require("../app");
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
 describe("POST /", function () {
   test("valid", async function () {
@@ -12,7 +17,7 @@ describe("POST /", function () {
       addr: "100 Test St",
       zip: "12345-6789",
     });
-
+    console.log("##################",resp.body)
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
   
@@ -27,3 +32,20 @@ describe("POST /", function () {
     expect(resp.statusCode).toEqual(400);
   });
 });
+
+// describe("POST /", function () {
+//   test("valid", async function (){
+//     axiosMock.onPost(`/shipments`)
+//              .reply(200, { shipped: 5000 });
+    
+//     const resp = await request(app).post("/shipments").send({
+//       productId: 1000,
+//       name: "Test Tester",
+//       addr: "100 Test St",
+//       zip: "12345-6789",
+//     });
+//     console.log("########################",resp)
+//     expect(resp).toEqual(5000);
+//   });
+
+// });
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,37 @@
 "use strict";
 
 const {
-  shipProduct,
+  shipProduct, SHIPIT_SHIP_URL
 } = require("./shipItApi");
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
+
+// test("shipProduct", async function () {
+//   const shipId = await shipProduct({
+//     productId: 1000,
+//     name: "Test Tester",
+//     addr: "100 Test St",
+//     zip: "12345-6789",
+//   });
+
+//   expect(shipId).toEqual(expect.any(Number));
+// });
 
 test("shipProduct", async function () {
+  axiosMock.onPost(SHIPIT_SHIP_URL)
+           .reply(200, { receipt:{shipId: 1234 }});
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
     zip: "12345-6789",
   });
-
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(1234);
 });
+
